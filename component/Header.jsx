5aa5279168dd1657e853632bd45b5c62d8d10a87
shariyerShazan <shariyershazan1@gmail.com>
2025-08-22 23:23:59 +0600

    updated

--- component/Header.jsx
+++ component/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import blog from "../public/blog.avif"
 const Header = () => {
   return (
-    <div className='px-5  md:-x-12 lg:px-24'>
+    <div className='px-5 md:-x-12 lg:px-24 '>
        <div className='flex justify-between items-center'>
-          <Image src={blog} width={180} alt='' className='w-[130px] sm:w-auto bg-transparent object-cover object-bottom'/>
-           <button className="flex cursor-pointer items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border-black border-1 shadow-[-7px_7px_0px_#000] ">
-              Get started
-           </button>
-       </div>
+                           <Image src={blog} width={100} alt='' className='scale-175 w-[80px] sm:w-auto bg-transparent object-cover object-bottom'/>
+                            <button className="flex cursor-pointer items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border-black border-1 shadow-[-7px_7px_0px_#000] ">
+                               Get started
+                            </button>
+                        </div>
        <div className='text-center my-8 '>
         <h1 className='text-3xl sm:text-5xl font-medium '>Latest blog</h1>
         <p className='mt-10 max-w-[740px] mx-auto text-sm sm:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo pariatur officiis corporis repellat aperiam ab ipsam voluptatibus eveniet fuga incidunt!</p>
--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import blogData from '../../../../Assets/data'
+import { useParams } from 'next/navigation'
+import blog from "../../../../public/blog.avif"
+import Image from 'next/image'
 
-const page = ({params}) => {
-    const 
+const page = () => {
+  const {id }= useParams()
+    const data = blogData.find((d)=> (d.id.toString() === id))
+
   return (
-    <div>
-      {params.id}
+    <div className='w-[90%] mx-auto'>
+          <div className='flex justify-between items-center '>
+                    <Image src={blog} width={100} alt='' className='scale-175 w-[80px] sm:w-auto bg-transparent object-cover object-bottom'/>
+                     <button className="flex cursor-pointer items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border-black border-1 shadow-[-7px_7px_0px_#000] ">
+                        Get started
+                     </button>
+                 </div>
+
+                 <div className='mt-12 h-[40vh]'>
+                     <h2 className='text-center text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data?.title}</h2>
+                     <Image src={data?.authorImage} alt='' width={100} height={100} className='mx-auto mt-12 border border-white rounded-full'/>
+                     <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto text-center font-bold'>{data.author}</p>
+                 </div>
+                <div className='mx-5 max-w-[800px] md:mx-auto mt-[-120px] mb-10 '>
+                        <Image src={data?.image} alt='' width={1280} height={720} className='border-4 border-white'/>
+                </div>
     </div>
   )
 }
