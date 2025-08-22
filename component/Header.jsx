69de16c249e26d97af98b064d31e4f1576f76e70
shariyerShazan <shariyershazan1@gmail.com>
2025-08-22 17:58:48 +0600

    home section build

---
+++ component/Header.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+import blog from "../public/blog.avif"
+const Header = () => {
+  return (
+    <div className='px-5  md:-x-12 lg:px-24'>
+       <div className='flex justify-between items-center'>
+          <Image src={blog} width={180} alt='' className='w-[130px] sm:w-auto bg-transparent object-cover object-bottom'/>
+           <button className="flex cursor-pointer items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border-black border-1 shadow-md ">
+              Get started
+           </button>
+       </div>
+       <div className='text-center my-8 '>
+        <h1 className='text-3xl sm:text-5xl font-medium'>Latest blog</h1>
+        <p className='mt-10 max-w-[740px] mx-auto text-sm sm:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo pariatur officiis corporis repellat aperiam ab ipsam voluptatibus eveniet fuga incidunt!</p>
+        <form action="" className='flex justify-between max-w-[500px] scale-75 mx-auto mt-10 border-black border'>
+            <input type="email" placeholder='Enter Your Email' className='pl-4 outline-none ' />
+            <button className='border-1 border-black py-4 px-4 sm:px-8 active:bg-gray-700 active:text-white'>Subscribe</button>
+        </form>  
+       </div> 
+    </div>
+  )
+}
+
+export default Header
