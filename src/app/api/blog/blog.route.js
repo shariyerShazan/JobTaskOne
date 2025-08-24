82c870304e3a49f5229c53759456d9ce70398a6b
shariyerShazan <shariyershazan1@gmail.com>
2025-08-24 15:52:06 +0600

    post blog data

---
+++ src/app/api/blog/blog.route.js
@@ -0,0 +1,56 @@
+import { NextResponse } from "next/server";
+import { connetDB } from "../../../../lib/config/db";
+import { v2 as cloudinary } from "cloudinary";
+import { Blog } from "../../../../lib/models/blog.model";
+
+const runDB = async ()=>{
+    await connetDB()
+}
+runDB()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+export async function GET(request){
+    return NextResponse.json()
+}
+
+export async function POST(req) {
+    try {
+      const formData = await req.formData();
+      const file = formData.get("image");
+  
+      if (!file) {
+        return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+      }
+  
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+  
+      const result = await new Promise((resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream({ folder: "uploads" }, (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+          })
+          .end(buffer);
+      });
+      const blogData = {
+        title : `${formData.get("title")}`,
+        description : `${formData.get("description")}`,
+        category : `${formData.get("category")}`,
+        author : `${formData.get("author")}`,
+        image : `${result.secure_url}`,
+        authorImage : `${formData.get("authorImage")}`
+      }
+  
+      await Blog.create(blogData)
+    } catch (error) {
+      console.error("Upload error:", error);
+      return NextResponse.json({ error: "Upload failed" }, { status: 500 });
+    }
+  }
+  
